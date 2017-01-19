355df71106f1d17d709255914bad17172e0760e1
Mitchel Mayle <mitchel.mayle@gmail.com>
2017-01-18 21:23:29 -0500

    Updated a lot, added notes for new sections

--- Computer Wars/Computer Wars/Program.cs
+++ Computer Wars/Computer Wars/Program.cs
@@ -10,6 +10,17 @@ namespace Computer_Wars
     {
         static void Main(string[] args)
         {
+            /* TO DO
+               - Develop system for random events in between days
+                    -- neighbors stealing items
+                    -- neighbors stealing identity
+                    -- prices for items going up/down
+                    -- bank returning high interest
+                    -- killed in plane crash
+                    -- flight delayed, lose a day
+                    -- specific city events?
+            */
+
             Dictionary<string, int> inventory = new Dictionary<string, int>()
             {
                 {"Processors", 0},
@@ -19,10 +30,45 @@ namespace Computer_Wars
                 {"Flash Drives", 0}
             };
 
-            int bankAccount = 500;
-            int dayCount = 1;
+            // create method to generate price values and class to hold current price values
+            Random changePrice = new Random();
+            int cpuPrice = changePrice.Next(1500, 4001);
+            int gpuPrice = changePrice.Next(601, 1601);
+            int hddPrice = changePrice.Next(201, 701);
+            int ramPrice = changePrice.Next(80, 301);
+            int fshPrice = changePrice.Next(10, 101);
 
-            //beginning of game
+            Dictionary<string, int> partsList = new Dictionary<string, int>()
+            {
+                {"Processors", cpuPrice},
+                {"Graphics Cards", gpuPrice},
+                {"Hard Drives",hddPrice},
+                {"RAM Cards",ramPrice},
+                {"Flash Drives", fshPrice}
+            };
+
+            //variable declaration
+            int bankAccount = 500;
+            int depositAccount = 0;
+            int dayCount = 1;
+            string currentCity = "Cleveland";
+            bool hasGambled = false;
+
+            //method to display title?
+            Console.WriteLine();
+            Console.WriteLine("  ██████╗ ██████╗ ███╗   ███╗██████╗ ██╗   ██╗████████╗███████╗██████╗     ██╗    ██╗ █████╗ ██████╗ ███████╗██╗");
+            Console.WriteLine(" ██╔════╝██╔═══██╗████╗ ████║██╔══██╗██║   ██║╚══██╔══╝██╔════╝██╔══██╗    ██║    ██║██╔══██╗██╔══██╗██╔════╝██║");
+            Console.WriteLine(" ██║     ██║   ██║██╔████╔██║██████╔╝██║   ██║   ██║   █████╗  ██████╔╝    ██║ █╗ ██║███████║██████╔╝███████╗██║");
+            Console.WriteLine(" ██║     ██║   ██║██║╚██╔╝██║██╔═══╝ ██║   ██║   ██║   ██╔══╝  ██╔══██╗    ██║███╗██║██╔══██║██╔══██╗╚════██║╚═╝");
+            Console.WriteLine(" ╚██████╗╚██████╔╝██║ ╚═╝ ██║██║     ╚██████╔╝   ██║   ███████╗██║  ██║    ╚███╔███╔╝██║  ██║██║  ██║███████║██╗");
+            Console.WriteLine("  ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚═╝      ╚═════╝    ╚═╝   ╚══════╝╚═╝  ╚═╝     ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝\n");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("***** PRESS ANY KEY TO START *****");
+            Console.ReadKey();
+            Console.Clear();
+
+            // introduction, add more description and hazards
             Console.WriteLine();
             Console.WriteLine("  ██████╗ ██████╗ ███╗   ███╗██████╗ ██╗   ██╗████████╗███████╗██████╗     ██╗    ██╗ █████╗ ██████╗ ███████╗██╗");
             Console.WriteLine(" ██╔════╝██╔═══██╗████╗ ████║██╔══██╗██║   ██║╚══██╔══╝██╔════╝██╔══██╗    ██║    ██║██╔══██╗██╔══██╗██╔════╝██║");
@@ -31,11 +77,25 @@ namespace Computer_Wars
             Console.WriteLine(" ╚██████╗╚██████╔╝██║ ╚═╝ ██║██║     ╚██████╔╝   ██║   ███████╗██║  ██║    ╚███╔███╔╝██║  ██║██║  ██║███████║██╗");
             Console.WriteLine("  ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚═╝      ╚═════╝    ╚═╝   ╚══════╝╚═╝  ╚═╝     ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝\n");
 
-            Console.WriteLine("Press any key to begin.");
-            Console.ReadKey();
+            Console.WriteLine();
+            Console.WriteLine("Your will be given 30 days to buy and sell computer parts to accumulate as much money as you can.\n");
+            Console.WriteLine("When you travel to a new location, one day will pass, and current prices of parts will change.");
+            Console.WriteLine("When you purchase parts, they will be magically transported to your house......");
+            Console.WriteLine("But BEWARE! Your neighbors are theives and can steal all of your parts from your house at any time during your travels.\n");
 
+            Console.WriteLine("Every city has a casino where you can gamble once a day after Day 1 to try to win more money.\n");
+
+            Console.WriteLine("You can also visit the bank to deposit money starting on Day 3 to attempt to gain interest on your deposit......");
+            Console.WriteLine("But BEWARE! If your theiving neighbors steal your identity, you could lose all of your money in your account.\n");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("***** PRESS ANY KEY TO BEGIN DAY 1 *****");
+
+            //pause
+            Console.ReadKey();
             Console.Clear();
 
+            // method to display graphic for each day?
             Console.WriteLine();
             Console.WriteLine(" ██████╗  █████╗ ██╗   ██╗     ██╗");
             Console.WriteLine(" ██╔══██╗██╔══██╗╚██╗ ██╔╝    ███║");
@@ -45,35 +105,63 @@ namespace Computer_Wars
             Console.WriteLine(" ╚═════╝ ╚═╝  ╚═╝   ╚═╝        ╚═╝ \n");
 
 
-            // create method to hold this information to begin new day
-            Console.WriteLine($"Weclome to day {dayCount}! \n");
-            Console.WriteLine($"Your balance is ${bankAccount} \n");
+            // create method to hold this information to begin new day, calculate interest at beginning of day, set bools back
+            Console.WriteLine($"Welcome to {currentCity}! You are currently on Day {dayCount} of 30.\n");
+            Console.WriteLine($"Your balance is ${bankAccount}.\n");
+            Console.WriteLine($"Your bank account contains ${depositAccount}.\n");
             Console.WriteLine($"Please make a selection:\n");
 
             // create method to display choices
-            Console.WriteLine("1. Check prices of items");
-            Console.WriteLine("2. Check inventory");
-            Console.WriteLine("3. Buy items");
-            Console.WriteLine("4. Sell items");
-            Console.WriteLine("5. Travel to a new city");
-            Console.WriteLine("6. Apply for a loan");
-            Console.WriteLine("7. Go to the bank");
+            Console.WriteLine("1. Check today's prices of parts");
+            Console.WriteLine("2. Check inventory at your house");
+            Console.WriteLine("3. Buy parts");
+            Console.WriteLine("4. Sell parts");
+            Console.WriteLine("5. Deposit money at bank");
+            Console.WriteLine("6. Gamble at the casino");
+            Console.WriteLine("7. Fly to a new city");
 
-            // create method to generate price values
-            Random changePrice = new Random();
+            //pause
+            Console.ReadKey();
+            Console.Clear();
+
+            // create method to display prices
+            Console.WriteLine("Current part prices:\n");
+
+            foreach (KeyValuePair<string, int> kvp in partsList)
+            {
+                Console.WriteLine($"{kvp.Key.PadRight(17)}-{kvp.Value.ToString().PadLeft(5)}");
+            }
+            Console.WriteLine("\n***** PRESS ANY KEY TO RETURN TO THE MENU *****");
+
+            //pause
+            Console.ReadKey();
+            Console.Clear();
 
             // create method to display inventory
             Console.Clear();
 
-            Console.WriteLine("You currently own the following items:\n");
-            
-            foreach(KeyValuePair<string, int> kvp in inventory)
+            Console.WriteLine("You currently have the following parts at your house:\n");
+
+            foreach (KeyValuePair<string, int> kvp in inventory)
             {
                 Console.WriteLine($"{kvp.Key.PadRight(17)}-{kvp.Value.ToString().PadLeft(5)}");
             }
 
-            Console.WriteLine("\nPress any key to return to the main menu");
+            Console.WriteLine("\n***** PRESS ANY KEY TO RETURN TO THE MENU *****");
 
+            //pause
+            Console.ReadKey();
+            Console.Clear();
+
+            // create method to display cities
+            Console.WriteLine("Select a new city in which to travel:\n");
+            Console.WriteLine("1. Cleveland");
+            Console.WriteLine("2. Pittsburgh");
+            Console.WriteLine("3. Chicago");
+            Console.WriteLine("4. Atlanta");
+            Console.WriteLine("5. Seattle");
+            Console.WriteLine("6. Las Vegas");
+            Console.WriteLine("7. Los Angeles");
         }
     }
 }
